d96e6f8c1bcf0a10c75699da5da36e8b40e61057
youssefbensalah07 <youssefbensalah963@gmail.com>
2024-07-19 16:23:06 +0200

    commit test

---
+++ src/Exmpl.java
@@ -0,0 +1,11 @@
+public class Exmpl {
+    String brand;
+    int price;
+    static String name;
+ public void show(){
+     System.out.println(brand+" "+ price +" "+name);
+ }
+     static void show1(Exmpl A1){
+        System.out.println(A1.brand+" "+ A1.price +" "+name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ class A extends Thread {
+    public void run(){
+        for (int i = 1; i <= 100; i++) {
+           // System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
+    class B extends Thread {
+        public void run(){
+            for (int i = 1; i <= 100; i++) {
+                //System.out.println("hi");
+                try {
+                    Thread.sleep(2);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            }
+        }
+
+        public class Main {
+            public static void main(String[] args) {
+
+
+                List<Integer> nums = Arrays.asList(1, 8, 7, 6, 3,4);
+
+                Stream<Object> result = nums.stream()
+                        .filter(n -> n%2 == 0 )
+                        .map(n ->n*2 );
+
+
+                result.forEach(n-> System.out.println(n));
+
+
+
+                //data.forEach(System.out::println);
+
+
+                int[][] num = new int[3][4];
+
+                for (int i = 0; i < num.length; i++) {
+
+                    for (int j = 0; j < num[i].length; j++) {
+
+                        // double v = Math.random() * 10;
+                      //  num[i][j] = (int) (Math.random() * 2);
+
+                    }
+
+                }
+                for (int i = 0; i < num.length; i++) {
+
+                    for (int j = 0; j < num[i].length; j++) {
+
+                        //System.out.print(" " + num[i][j]);
+
+
+                    }
+                    //System.out.println(" ");
+
+
+                }
+                //Exmpl A1 = new Exmpl();
+               // A1.brand = "Ios";
+               // A1.price = 2000;
+               // Exmpl.name = "smart";
+                // A1.show();
+                //Exmpl.show1(A1);
+            A obj1 =new A();
+            B obj2 =new B();
+
+            //obj1.start();
+              try {
+                  Thread.sleep(2);
+              }catch (InterruptedException e){e.printStackTrace();}
+
+            //obj2.start();
+
+
+
+            }
+        }
+
+
+
+
+
+
+
